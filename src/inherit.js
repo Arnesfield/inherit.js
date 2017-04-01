ef2067d5d6ae3d4d32555fd31f645e912a4ce6a9
Jefferson Rylee <Arnesfield@users.noreply.github.com>
2017-04-01 14:24:43 +0800

    Update inherit.js

--- src/inherit.js
+++ src/inherit.js
@@ -1,6 +1,7 @@
+/* inherit.js v1.1 | Jefferson Rylee - https://github.com/Arnesfield/inherit.js | Free */
+
 // inherit.js requires jQuery and the Regex Selector for jQuery by James Padolsey
 // https://gist.github.com/fny/1887398
-// credits to James Padolsey for the plugin
 
 //self invoking function
 (function() {
@@ -15,18 +16,22 @@
     });
     return c;
   }
+  
   var x = ':regex(class,(^-)|( -))',
       y = ':regex(class,(^no-)|( no-))';
+  
   // inherits classes based on "-"
   while ($(x).length) {
     $(x).each(function() {
       $(this).children().not('.no-').addClass(get($(this), 1, /^-/));
     });
   }
+  
   // remove classes that starts with "no-"
   $(y).each(function() {
     $(this).removeClass(get($(this), 3, /^no-/));
   });
+  
   // remove all empty class attributes
   $('*[class=""]').removeAttr('class');
 })();
